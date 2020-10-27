ffaef72498c3dd7aa0a2a332164e2c88a134fa7b
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2020-10-27 09:13:39 -0300

    Creada Clase Profesor

---
+++ VarelaVargas.Leandro.2D.TP3/Clases Instanciables/Profesor.cs
@@ -0,0 +1,22 @@
+﻿using EntidadesAbstractas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Clases_Instanciables.Universidad;
+
+namespace Clases_Instanciables
+{
+    public sealed class Profesor : Universitario
+    {
+
+        #region Campos
+        private Queue<EClases> clasesDelDia;
+        private static Random random;
+        #endregion
+
+
+
+    }
+}
