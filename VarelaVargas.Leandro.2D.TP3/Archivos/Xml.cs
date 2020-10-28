f1e3c29c7f4851d897e00aef6d2b32e089a7d003
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2020-10-28 09:24:07 -0300

    Clase Xml y ArchivosException creadas y finalizadas.

---
+++ VarelaVargas.Leandro.2D.TP3/Archivos/Xml.cs
@@ -0,0 +1,56 @@
+﻿using Excepciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Archivos
+{
+    public class Xml<T> : IArchivo<T>
+    {
+        public bool Guardar(string archivo, T datos)
+        {
+            bool ret = false;
+
+            try
+            {
+                using (XmlTextWriter xmlWriter = new XmlTextWriter(archivo, Encoding.UTF8))
+                {
+                    XmlSerializer serializer = new XmlSerializer((typeof(T)));
+                    serializer.Serialize(xmlWriter, datos);
+                    ret = true;
+                }
+            }
+            catch ( Exception ex )
+            {
+                throw new ArchivosException(ex);
+            }
+
+            return ret;
+        }
+
+        public bool Leer(string archivo, T datos)
+        {
+            bool ret = false;
+
+            try
+            {
+                using (XmlTextReader reader = new XmlTextReader(archivo))
+                {
+                    XmlSerializer serializer = new XmlSerializer((typeof(T)));
+                    datos = (T)serializer.Deserialize(reader);
+                    ret = true;
+                }
+            }
+            catch ( Exception ex )
+            {
+                throw new ArchivosException(ex);
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ VarelaVargas.Leandro.2D.TP3/Excepciones/ArchivosException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class ArchivosException : Exception
+    {
+
+        public ArchivosException()
+            : base ("No se ha podido crear el archivo.")
+        {}
+
+        public ArchivosException ( string message )
+            : base ( message )
+        {}
+
+        public ArchivosException(Exception innerException)
+            : base("No se ha podido crear el archivo.", innerException)
+        {}
+
+    }
+}
