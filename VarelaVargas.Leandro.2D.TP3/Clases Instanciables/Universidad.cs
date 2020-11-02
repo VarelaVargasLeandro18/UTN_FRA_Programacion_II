3ded2b72a3a5d89212ac6e2021b0aec21148cc11
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2020-11-02 02:37:43 -0300

    Documentación Universidad.[C

--- VarelaVargas.Leandro.2D.TP3/Clases Instanciables/Universidad.cs
+++ VarelaVargas.Leandro.2D.TP3/Clases Instanciables/Universidad.cs
@@ -10,9 +10,16 @@ using System.Threading.Tasks;
 
 namespace Clases_Instanciables
 {
+
+    /// <summary>
+    /// Clase Universidad. Contendrá Alumnos, Jornadas y Profesores que participan en ella.
+    /// </summary>
     public class Universidad
     {
 
+        /// <summary>
+        /// Clases que da la Universidad.
+        /// </summary>
         public enum EClases
         {
             Programacion,
@@ -38,6 +45,9 @@ namespace Clases_Instanciables
 
         #region Propiedades
 
+        /// <summary>
+        /// Propiedad Alumnos. Obtiene y Settea la lista de Alumnos que pertenecen a la Universidad.
+        /// </summary>
         public List<Alumno> Alumnos
         {
             get
@@ -51,6 +61,9 @@ namespace Clases_Instanciables
             }
         }
 
+        /// <summary>
+        /// Propiedad Instructores. Obtiene y Settea la lista de Profesores que pertenecen a la Universidad.
+        /// </summary>
         public List<Profesor> Instructores
         {
             get
@@ -64,6 +77,9 @@ namespace Clases_Instanciables
             }
         }
 
+        /// <summary>
+        /// Propiedad Jornadas. Obtiene y Settea la lista de Jornadas que tiene la Universidad como Programa.
+        /// </summary>
         public List<Jornada> Jornadas
         {
             get
@@ -78,6 +94,12 @@ namespace Clases_Instanciables
         }
 
         #region Indexador
+
+        /// <summary>
+        /// Indexador Jornada. Devuelve una Jornada que se encuentra en la posición especificada de la lista de Jornadas.
+        /// </summary>
+        /// <param name="i">Index de jornada.</param>
+        /// <returns>Devuelve la Jornada.</returns>
         public Jornada this[int i]
         {
 
@@ -171,16 +193,35 @@ namespace Clases_Instanciables
         #region Operadores
 
         #region Universidad - Alumno
+
+        /// <summary>
+        /// Comprobará que un Alumno pertenezca a una Universidad.
+        /// </summary>
+        /// <param name="g">Universidad en la que se comprobará que pertenezca el alumno especificado.</param>
+        /// <param name="a">Alumno que se comprobará que exista en la Universidad.</param>
+        /// <returns></returns>
         public static bool operator ==(Universidad g, Alumno a)
         {
             return g.Alumnos.Contains(a);
         }
 
+        /// <summary>
+        /// Comprobará que un Alumno NO pertenezca a una Universidad.
+        /// </summary>
+        /// <param name="g">Universidad donde se comprobará que el Alumno NO exista.</param>
+        /// <param name="a">Alumno que se comprobará que NO exista en una Universidad.</param>
+        /// <returns>True si el Alumno no existe. Caso contrario False.</returns>
         public static bool operator !=(Universidad g, Alumno a)
         {
             return !(g == a);
         }
-            
+        
+        /// <summary>
+        /// Crea una copia de la Universidad 'u' y le agrega un alumno (de ser posible).
+        /// </summary>
+        /// <param name="u">Universidad que se copiará.</param>
+        /// <param name="a">Alumno a agregar.</param>
+        /// <returns>Nueva Universidad con el alumno agregado.</returns>
         public static Universidad operator +(Universidad u, Alumno a)
         {
             Universidad ret = new Universidad();
@@ -199,16 +240,35 @@ namespace Clases_Instanciables
         #endregion
 
         #region Universidad - Profesor
+
+        /// <summary>
+        /// Comprueba que un profesor pertenezca a una Universidad.
+        /// </summary>
+        /// <param name="g">La Universidad donde se comprobará que el profesor pertenece.</param>
+        /// <param name="i">Profesor que se comprobará que pertenezca a la Universidad.</param>
+        /// <returns>True si pertenece. False si no pertenece.</returns>
         public static bool operator ==(Universidad g, Profesor i)
         {
             return g.profesores.Contains(i);
         }
 
+        /// <summary>
+        /// Comprueba que un profesor NO pertenezca a una Universidad.
+        /// </summary>
+        /// <param name="g">Universidad donde se comprobará que el profesor NO pertenece.</param>
+        /// <param name="i">Profesor que se comprobará que NO forma parte de la Universidad.</param>
+        /// <returns>True si NO pertenece. False si sí.</returns>
         public static bool operator !=(Universidad g, Profesor i)
         {
             return !(g == i);
         }
 
+        /// <summary>
+        /// Copia una Universidad y devuelvé otra con un profesor cargado (en caso de que no lo contenga).
+        /// </summary>
+        /// <param name="u">Universidad a la que se agregará el profesor.</param>
+        /// <param name="i">Profesor a agregar en la Universidad.</param>
+        /// <returns>Copia de la Universidad 'u' con un nuevo profesor.</returns>
         public static Universidad operator +(Universidad u, Profesor i)
         {
             Universidad ret = new Universidad();
@@ -225,6 +285,13 @@ namespace Clases_Instanciables
         #endregion
 
         #region Universidad - EClases
+
+        /// <summary>
+        /// Busca un profesor que dé la clase dentro de una Universidad.
+        /// </summary>
+        /// <param name="u">Universidad donde se buscará la clase.</param>
+        /// <param name="clase">Clase que debe dar el profesor.</param>
+        /// <returns>Profesor que dá la clase.</returns>
         public static Profesor operator ==(Universidad u, EClases clase)
         {
             Profesor ret = null;
@@ -244,6 +311,12 @@ namespace Clases_Instanciables
             return ret;
         }
 
+        /// <summary>
+        /// Busca el primero profesor que no dé la clase dentro de una Universidad.
+        /// </summary>
+        /// <param name="u">Universidad donde se buscará un profesor que no dé la clase.</param>
+        /// <param name="clase">Clase que no debe dar un profesor.</param>
+        /// <returns></returns>
         public static Profesor operator !=(Universidad u, EClases clase)
         {
             Profesor ret = null;
@@ -260,6 +333,13 @@ namespace Clases_Instanciables
             return ret;
         }
         
+        /// <summary>
+        /// Crea una nueva jornada con un profesor que dé la clase especificada y una lista de alumnos que tomen dicha clase.
+        /// Por consiguiente se creará una copia de la universidad 'g' con la Jornada agregada.
+        /// </summary>
+        /// <param name="g">Universidad que se copiará.</param>
+        /// <param name="clase">Clase que se buscará que los profesores y alumnos tomen.</param>
+        /// <returns>Devuelve una Universidad con la jornada creada. Si no se puede crear la jornada devuelve una copia de 'g'</returns>
         public static Universidad operator +(Universidad g, EClases clase)
         {
             Universidad ret = null;
